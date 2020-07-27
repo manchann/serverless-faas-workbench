59a558f53ed4fcf91e1d7b6165b98ff7d0525cf6
cjg9040 <chl8273@naver.com>
2020-07-27 20:54:30 +0900

    add comment

--- aws/efs/test/efs-scalability.sh
+++ aws/efs/test/efs-scalability.sh
@@ -1,9 +1,11 @@
+#remove dynamodb datas
 python3 /Users/manchan/Desktop/programming/serverless-faas-workbench/aws/dynamodb/dynamodb_all_remove.py
 sleep 10
-
+#change lambda_function sequence -> random
 aws lambda update-function-configuration --function-name efs-test --handler random.lambda_handler
 sleep 40
 
+#start random access work
 python3 ./efs_scalablity/request1.py
 sleep 15
 python3 ./efs_scalablity/request10.py
@@ -17,6 +19,7 @@ sleep 100
 python3 ./efs_scalablity/request200.py
 sleep 100
 
+#change lambda_function random -> sequence
 aws lambda update-function-configuration --function-name efs-test --handler sequence.lambda_handler
 sleep 60
 
@@ -33,6 +36,7 @@ sleep 100
 python3 ./efs_scalablity/request200.py
 sleep 100
 
+#change lambda_function sequence -> dd
 aws lambda update-function-configuration --function-name efs-test --handler dd.lambda_handler
 sleep 40
 
@@ -48,4 +52,6 @@ python3 ./efs_scalablity/request100.py
 sleep 100
 python3 ./efs_scalablity/request200.py
 sleep 100
+
+#export dynamodb data to local json file
 sh /Users/manchan/Desktop/programming/serverless-faas-workbench/aws/dynamodb/dynamodb_export_json.sh EFS /Users/manchan/Desktop/BigDataLab/Papers/efs-scalablity
