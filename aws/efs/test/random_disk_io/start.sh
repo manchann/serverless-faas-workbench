8786de2c8c020c7973c3175c04f28df7f76599c8
cjg9040 <chl8273@naver.com>
2020-07-29 14:27:18 +0900

    lambda memory 변경도 자동화

--- aws/efs/test/random_disk_io/start.sh
+++ aws/efs/test/random_disk_io/start.sh
@@ -1,134 +1,68 @@
-python3 /Users/manchan/Desktop/programming/serverless-faas-workbench/aws/dynamodb/dynamodb_all_remove.py
+#!/bin/bash
 
-aws lambda update-function-configuration --function-name efs-test --handler random-write.lambda_handler --memory-size 2048
-aws lambda update-function-configuration --function-name tmp-test --handler random-write.lambda_handler --memory-size 2048
+lambda_memory="512 2048"
 
-sleep 70
-python3 ./request.py
-sleep 60
-python3 ./request.py
-sleep 60
-python3 ./request.py
-sleep 60
-python3 ./request.py
-sleep 60
-python3 ./request.py
-sleep 60
+for lm in $lambda_memory; do
+  python3 /Users/manchan/Desktop/programming/serverless-faas-workbench/aws/dynamodb/dynamodb_all_remove.py
 
-aws lambda update-function-configuration --function-name efs-test --handler sequence-write.lambda_handler
-aws lambda update-function-configuration --function-name tmp-test --handler sequence-write.lambda_handler
+  aws lambda update-function-configuration --function-name efs-test --handler random-write.lambda_handler --memory-size $lm
+  aws lambda update-function-configuration --function-name tmp-test --handler random-write.lambda_handler --memory-size $lm
 
-sleep 60
-python3 ./request.py
-sleep 60
-python3 ./request.py
-sleep 60
-python3 ./request.py
-sleep 60
-python3 ./request.py
-sleep 60
-python3 ./request.py
-sleep 60
+  sleep 70
 
-aws lambda update-function-configuration --function-name efs-test --handler random-read.lambda_handler
-aws lambda update-function-configuration --function-name tmp-test --handler random-read.lambda_handler
+  for ((i = 0; i < 5; i++)); do
 
-sleep 10
-aws lambda update-function-configuration --function-name efs-test --memory-size 256
-aws lambda update-function-configuration --function-name tmp-test --memory-size 256
-sleep 5
-aws lambda update-function-configuration --function-name efs-test --memory-size 2048
-aws lambda update-function-configuration --function-name tmp-test --memory-size 2048
-sleep 40
+    python3 ./request.py
+    sleep 60
 
-python3 ./request.py
-sleep 60
+  done
 
-aws lambda update-function-configuration --function-name efs-test --memory-size 256
-aws lambda update-function-configuration --function-name tmp-test --memory-size 256
-sleep 5
-aws lambda update-function-configuration --function-name efs-test --memory-size 2048
-aws lambda update-function-configuration --function-name tmp-test --memory-size 2048
-sleep 30
+  aws lambda update-function-configuration --function-name efs-test --handler sequence-write.lambda_handler
+  aws lambda update-function-configuration --function-name tmp-test --handler sequence-write.lambda_handler
 
-python3 ./request.py
-sleep 60
+  sleep 60
+  for ((i = 0; i < 5; i++)); do
 
-aws lambda update-function-configuration --function-name efs-test --memory-size 256
-aws lambda update-function-configuration --function-name tmp-test --memory-size 256
-sleep 5
-aws lambda update-function-configuration --function-name efs-test --memory-size 2048
-aws lambda update-function-configuration --function-name tmp-test --memory-size 2048
-sleep 30
+    python3 ./request.py
+    sleep 60
 
-python3 ./request.py
-sleep 60
+  done
 
-aws lambda update-function-configuration --function-name efs-test --memory-size 256
-aws lambda update-function-configuration --function-name tmp-test --memory-size 256
-sleep 5
-aws lambda update-function-configuration --function-name efs-test --memory-size 2048
-aws lambda update-function-configuration --function-name tmp-test --memory-size 2048
-sleep 30
+  aws lambda update-function-configuration --function-name efs-test --handler random-read.lambda_handler
+  aws lambda update-function-configuration --function-name tmp-test --handler random-read.lambda_handler
 
-python3 ./request.py
-sleep 60
+  for ((i = 0; i < 5; i++)); do
 
-aws lambda update-function-configuration --function-name efs-test --memory-size 256
-aws lambda update-function-configuration --function-name tmp-test --memory-size 256
-sleep 5
-aws lambda update-function-configuration --function-name efs-test --memory-size 512
-aws lambda update-function-configuration --function-name tmp-test --memory-size 512
-sleep 30
+    sleep 10
+    aws lambda update-function-configuration --function-name efs-test --memory-size 256
+    aws lambda update-function-configuration --function-name tmp-test --memory-size 256
+    sleep 5
+    aws lambda update-function-configuration --function-name efs-test --memory-size $lm
+    aws lambda update-function-configuration --function-name tmp-test --memory-size $lm
+    sleep 40
 
-python3 ./request.py
-sleep 60
+    python3 ./request.py
+    sleep 60
 
-aws lambda update-function-configuration --function-name efs-test --handler sequence-read.lambda_handler
-aws lambda update-function-configuration --function-name tmp-test --handler sequence-read.lambda_handler
+  done
 
-sleep 40
-python3 ./request.py
-sleep 60
+  aws lambda update-function-configuration --function-name efs-test --handler sequence-read.lambda_handler
+  aws lambda update-function-configuration --function-name tmp-test --handler sequence-read.lambda_handler
 
-aws lambda update-function-configuration --function-name efs-test --memory-size 256
-aws lambda update-function-configuration --function-name tmp-test --memory-size 256
-sleep 5
-aws lambda update-function-configuration --function-name efs-test --memory-size 2048
-aws lambda update-function-configuration --function-name tmp-test --memory-size 2048
-sleep 30
+  for ((i = 0; i < 5; i++)); do
 
-python3 ./request.py
-sleep 60
+    sleep 10
+    aws lambda update-function-configuration --function-name efs-test --memory-size 256
+    aws lambda update-function-configuration --function-name tmp-test --memory-size 256
+    sleep 5
+    aws lambda update-function-configuration --function-name efs-test --memory-size $lm
+    aws lambda update-function-configuration --function-name tmp-test --memory-size $lm
+    sleep 40
 
-aws lambda update-function-configuration --function-name efs-test --memory-size 256
-aws lambda update-function-configuration --function-name tmp-test --memory-size 256
-sleep 5
-aws lambda update-function-configuration --function-name efs-test --memory-size 2048
-aws lambda update-function-configuration --function-name tmp-test --memory-size 2048
-sleep 30
+    python3 ./request.py
+    sleep 60
 
-python3 ./request.py
-sleep 60
+  done
 
-aws lambda update-function-configuration --function-name efs-test --memory-size 256
-aws lambda update-function-configuration --function-name tmp-test --memory-size 256
-sleep 5
-aws lambda update-function-configuration --function-name efs-test --memory-size 2048
-aws lambda update-function-configuration --function-name tmp-test --memory-size 2048
-sleep 30
-
-python3 ./request.py
-sleep 60
-
-aws lambda update-function-configuration --function-name efs-test --memory-size 256
-aws lambda update-function-configuration --function-name tmp-test --memory-size 256
-sleep 5
-aws lambda update-function-configuration --function-name efs-test --memory-size 2048
-aws lambda update-function-configuration --function-name tmp-test --memory-size 2048
-sleep 30
-
-python3 ./request.py
-sleep 60
-
-sh /Users/manchan/Desktop/programming/serverless-faas-workbench/aws/dynamodb/dynamodb_export_json.sh EFS /Users/manchan/Desktop/BigDataLab/Papers/file_w_test-1
+  sh /Users/manchan/Desktop/programming/serverless-faas-workbench/aws/dynamodb/dynamodb_export_json.sh EFS /Users/manchan/Desktop/BigDataLab/Papers/file_rw_test-$lm
+done
